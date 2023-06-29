34ba11cdcb4c843f153cc8b5882de16520df1c38
TommasoPezzola <pezzola123@gmail.com>
2023-06-29 16:32:30 +0200

    error

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', [PageController::class, 'index']);
+
+/****  Route::get('/', function () {
     $links = config('store.someLinks');
     return view('welcome', compact('links'));
-});
+}); *****/
 
 Route::get('/otherpage', function () {
     $links = config('store.someLinks');
